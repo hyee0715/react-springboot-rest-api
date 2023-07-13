3530fc646e1e0c1116ee88d87ae508d6ba9c8870
hyee0715 <hyee0715@naver.com>
2023-07-14 00:52:03 +0900

    feat: 장바구니 view 구현

---
+++ gc-coffee-ui/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import React, {useState} from 'react';
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+
+function App() {
+    const [products, setProducts] = useState([
+        {id: 'uuid-1', productName: '콜롬비아 커피 1', cateogry:'커피빈', price: 5000},
+        {id: 'uuid-2', productName: '콜롬비아 커피 2', cateogry:'커피빈', price: 5000},
+        {id: 'uuid-3', productName: '콜롬비아 커피 3', cateogry:'커피빈', price: 5000},
+    ]);
+
+    const [items, setItems] = useState([]);
+    const handleAddClicked = id => {
+        const product = products.find(v => v.id === id);
+        const found = items.find(v => v.id === id);
+        const updatedItems =
+            found ? items.map(v => (v.id === id) ? {...v, count: v.count + 1} : v) : [...items, {...product, count: 1}]
+            setItems(updatedItems);
+
+            console.log(products.find(v => v.id === id), "added!");
+    }
+  return (
+      <div className="container-fluid">
+      <div className="row justify-content-center m-4">
+        <h1 className="text-center">Grids & Circle</h1>
+      </div>
+      <div className="card">
+        <div className="row">
+          <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+            <ProductList products={products} onAddClick={handleAddClicked}/>
+          </div>
+          <div className="col-md-4 summary p-4">
+            <Summary items={items}/>
+          </div>
+        </div>
+      </div>
+      </div>
+  );
+}
+
+export default App;
